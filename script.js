f07543de33c40649905c4bb9c3aedd4ff1b75494
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-04-24 21:29:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let result="";
+for(let i=1;i<=100;i++)
+	{
+		if(i%3==0)
+		{
+			result=result+"Fizz";
+		}
+		else if(i%5==0)
+		{
+			result=result+"Buzz";
+		}
+		else if(i%3==0 && i%5==0)
+		{
+			result=result+"FizzBuzz";
+		}
+		else{
+			result=result+i;
+		}
+	}
+alert(result);
